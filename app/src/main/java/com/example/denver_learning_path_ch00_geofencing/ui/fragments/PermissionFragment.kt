9c04e4c1e34d8533d79af55d12aa12389b14088f
Denver <gregoriomarkdenver01@gmail.com>
2024-08-25 04:05:12 +0800

    CH00: Create fragment layouts

---
+++ app/src/main/java/com/example/denver_learning_path_ch00_geofencing/ui/fragments/PermissionFragment.kt
@@ -0,0 +1,22 @@
+package com.example.denver_learning_path_ch00_geofencing.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class PermissionFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+}
---
+++ app/src/main/java/com/example/denver_learning_path_ch00_geofencing/ui/viewmodels/GeofenceViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.denver_learning_path_ch00_geofencing.ui.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class GeofenceViewModel(
+    application: Application
+) : AndroidViewModel(application) {
+
+}
